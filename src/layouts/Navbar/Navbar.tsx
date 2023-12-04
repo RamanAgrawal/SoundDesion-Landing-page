c3bb59874e1153c3c9119bed6df4a3b483351947
Raman Agrawal <iramanagrawal@gmail.com>
2023-12-04 21:45:19 +0530

    Mobile and tab view @Navbar

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,5 +1,10 @@
+import { closeIcon, menuIcon } from "../../assets";
+
+import {useState} from 'react'
 import "./style.scss";
 const Navbar = () => {
+
+  const [showNav, setShowNav] = useState(false)
   const navigations = [
     {
       name: "Home",
@@ -7,19 +12,20 @@ const Navbar = () => {
     },
     {
       name: "Courses",
-      link: "#",
+      link: "#topics",
     },
     {
       name: "About",
-      link: "#",
+      link: "#info",
     },
     {
       name: "Blog",
-      link: "#",
+      link: "#blog",
     },
   ];
 
   return (
+    <>
     <nav>
       <a href="#" className="logo">
         Sound <span>DZign</span>
@@ -32,7 +38,23 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      <div onClick={()=>setShowNav(true)} className="menu-icon">
+        <img src={menuIcon} alt="menu icon" />
+      </div>
     </nav>
+    <div className={`mobile-nav ${showNav ? 'active':""}`}>
+      <div onClick={()=>setShowNav(false)} className="close">
+        <img src={closeIcon} alt="closeicon" />
+      </div>
+      <ul>
+        {navigations.map((nav, index) => (
+          <li key={index}>
+            <a href={nav.link}>{nav.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+    </>
   );
 };
 
