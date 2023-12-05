2e98eacdc5f498733fd4c440201384026715d751
Raman Agrawal <iramanagrawal@gmail.com>
2023-12-05 11:52:38 +0530

    code refactor @Header

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,15 +1,25 @@
+// Importing required components and styles
 import Navbar from "../Navbar/Navbar";
 import "./style.scss";
 
+// Header functional component
 const Header = () => {
   return (
-    <header>
+    // Header section for the website
+    <header aria-label="Header" role="banner">
+      {/* Wrapper for header content */}
       <div className="wrapper">
-        <Navbar/>
-        <div className="cta">
+        <Navbar />
+        {/* Call-to-action section */}
+        <div className="cta" aria-label="Call-to-action">
+          {/* Course name */}
           <p className="course-name">Sounds Masterclass</p>
+          {/* Main heading */}
           <h1>Learn the art of sound design</h1>
-          <button className="cta-button">Demo Lesson</button>
+
+          <button className="cta-button" aria-label="Take a Demo Lesson">
+            Demo Lesson
+          </button>
         </div>
       </div>
     </header>
