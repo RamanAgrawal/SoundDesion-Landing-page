7b3796bfe8cd958e343dba10b5d534870d8607bb
Raman Agrawal <iramanagrawal@gmail.com>
2023-12-05 11:52:21 +0530

    code refactor @Footer

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,7 +1,22 @@
+// Importing styles and assets
 import "./style.scss";
 import { instagramLogo, facebookLogo, twitterLogo } from "../../assets";
+
+// Define type for social icon
+interface SocialIcon {
+  img: string;
+  link: string;
+}
+
+interface QuickLinkI {
+  text: string;
+  link: string;
+}
+
+// Footer functional component
 const Footer = () => {
-  const socialIcons = [
+  // Array of social icons
+  const socialIcons: SocialIcon[] = [
     {
       img: instagramLogo,
       link: "https://www.instagram.com/sounddzign/",
@@ -16,56 +31,78 @@ const Footer = () => {
     },
   ];
 
+  // Array of quick links
+  const quickLinks: QuickLinkI[] = [
+    {
+      text: "About",
+      link: "https://www.instagram.com/sounddzign/",
+    },
+    {
+      text: "Services",
+      link: "https://www.facebook.com/sounddzign",
+    },
+    {
+      text: "Contact",
+      link: "https://twitter.com/sounddzign",
+    },
+  ];
+
   return (
-    <footer>
+    // Footer section
+    <footer aria-label="Footer" role="contentinfo">
+      {/* Wrapper for footer content */}
       <div className="wrapper">
+        {/* Footer content container */}
         <div className="content-container">
+          {/* Company logo and social icons */}
           <div className="link">
+            {/* SEO: Use h2 for better structure */}
             <a href="#" className="logo">
               Sound <span>DZign</span>
             </a>
-            <div className="social-icons">
+            {/* Social icons */}
+            <div className="social-icons" aria-label="Social Links">
+              {/* Map through social icons and render images with links */}
               {socialIcons.map((socialIcon, i) => (
                 <a href={socialIcon.link} key={i}>
-                  <img src={socialIcon.img} alt="" />
+                  {/* Provide alt text for accessibility */}
+                  <img src={socialIcon.img} alt={`Social Icon ${i}`} />
                 </a>
               ))}
             </div>
-            <div className="copyright">
-              This Website is designed and developed by Raman @2023
-            </div>
+            {/* Copyright */}
+            <div className="copyright">Website developed by Raman @2023</div>
           </div>
+          {/* Quick Links */}
           <div className="link">
             <h3>Quick Links</h3>
             <ul>
-              <li>
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Services</a>
-              </li>
-
-              <li>
-                <a href="#">Contact</a>
-              </li>
+              {/* Map through quick links and create anchor links */}
+              {quickLinks.map((link, i) => (
+                <li key={i}>
+                  <a href={link.link}>{link.text}</a>
+                </li>
+              ))}
             </ul>
           </div>
-
+          {/* Contacts */}
           <div className="link">
-            <h3>Contects</h3>
+            <h3>Contact</h3>
             <ul>
+              {/* Provide contact information */}
               <li>
-                <a href="#">iramanagrawqal@gmail.com</a>
+                <a href="mailto:iramanagrawal@gmail.com">
+                  iramanagrawal@gmail.com
+                </a>
               </li>
               <li>
-                <a href="#">8959095100</a>
+                <a href="tel:8959095100">8959095100</a>
               </li>
             </ul>
           </div>
         </div>
-        <div className="mob copyright">
-          This Website is designed and developed by Raman @2023
-        </div>
+        {/* Mobile view copyright */}
+        <div className="mob copyright">Website developed by Raman @2023</div>
       </div>
     </footer>
   );
