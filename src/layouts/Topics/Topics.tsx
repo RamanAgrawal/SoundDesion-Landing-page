618e1eb539839e89e00856fe16c21ab941fcb5be
Raman Agrawal <iramanagrawal@gmail.com>
2023-12-05 11:53:33 +0530

    code refactor @Topic

--- src/layouts/Topics/Topics.tsx
+++ src/layouts/Topics/Topics.tsx
@@ -1,5 +1,5 @@
+import React, { ReactElement, useState } from "react";
 import "./style.scss";
-import { useState } from "react";
 import {
   daw,
   frequencies,
@@ -9,33 +9,67 @@ import {
   mastering,
   bgElement1,
 } from "../../assets";
-const Topics = () => {
+
+// Define the structure of each topic
+interface Topic {
+  text: string;
+  img: string;
+}
+
+const Topics: React.FC = (): ReactElement => {
+  // State to handle changing topic images
   const [topicImg, setTopicImg] = useState<string>(frequencies);
-  const topics = [
+
+  // List of topics with their respective images
+  const topics: Topic[] = [
     { text: "What are frequencies", img: frequencies },
-    { text: "Using the Dow", img: daw },
+    { text: "Using the DAW", img: daw },
     { text: "Vocals Processing", img: vocalsProcessing },
     { text: "Mixing", img: mixing },
-    { text: "MixingConsole", img: mixingConsole },
+    { text: "Mixing Console", img: mixingConsole },
     { text: "Mastering", img: mastering },
   ];
+
   return (
-    <section id="topics">
+    // Topics section with an aria-label for accessibility
+    <section id="topics" aria-label="Topics" role="region">
       <div className="wrapper">
-        <h2>What will You Learn..?</h2>
+        {/* Title for the topics section */}
+        <h2>What Will You Learn..?</h2>
         <div className="content-container">
+          {/* List of topics */}
           <ul>
-            {topics.map((topic, i) => (
+            {/* Map through topics and display as list items */}
+            {topics.map((topic: Topic, i: number) => (
               <li key={i} onMouseEnter={() => setTopicImg(topic.img)}>
                 {topic.text}
               </li>
             ))}
           </ul>
+          {/* Container to display the topic image */}
           <div className="topic-img">
             <img src={topicImg} alt={topicImg} />
           </div>
         </div>
-        <img src={bgElement1} className="ele1" alt="element" />
+        {/* Background element */}
+        <img
+          src={bgElement1}
+          className="ele1"
+          alt="Background Element"
+          useMap="#topicMap"
+        />
+        {/* Define a map for area tags */}
+        <map name="topicMap">
+          {topics.map((topic: Topic, i: number) => (
+            <area
+              key={i}
+              shape="rect"
+              coords="0,0,100,100"
+              href={`#${topic.text.toLowerCase().replace(/\s/g, "-")}`}
+              alt={topic.text}
+            />
+          ))}
+        </map>
       </div>
     </section>
   );
