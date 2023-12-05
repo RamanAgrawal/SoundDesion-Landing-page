ad5acf8d70534c5231252f59994c12a6d92c2887
Raman Agrawal <iramanagrawal@gmail.com>
2023-12-05 11:52:50 +0530

    code refactor @Info

--- src/layouts/Info/Info.tsx
+++ src/layouts/Info/Info.tsx
@@ -1,20 +1,29 @@
 import { bgElement2, studentIcon, videoIcon } from "../../assets";
 import "./style.scss";
 
+// Info functional component
 const Info = () => {
   return (
-    <section id="info">
+    // Section for information display
+    <section id="info" aria-label="Information" role="region">
+      {/* Wrapper for content */}
       <div className="wrapper">
-      <img src={bgElement2} className="ele2" alt="element" />
-        <div className="content-container">
-          <div className="info-content">
-            <img src={studentIcon} alt={studentIcon} />
+        {/* Background element */}
+        <img src={bgElement2} className="ele2" alt="Decorative element" />
+        {/* Content container */}
+        <div className="content-container" aria-label="Information content">
+          {/* Student information */}
+          <div className="info-content" aria-label="Student information">
+            <img src={studentIcon} alt="Student Icon" />
+            {/* Total number of students */}
             <div className="amount">23000+</div>
-            <div className="type">student</div>
+            {/* Type of information */}
+            <div className="type">Students</div>
           </div>
-          <div className="info-content">
-            <img src={videoIcon} alt={videoIcon} />
-            <div className="amount">26Hr</div>
+          {/* Video content information */}
+          <div className="info-content" aria-label="Video content information">
+            <img src={videoIcon} alt="Video Icon" />
+            <div className="amount">26 Hrs</div>
             <div className="type">Video Content</div>
           </div>
         </div>
