48cfaed4427f53cf0bd6e9caa69ab232042d3edd
Raman Agrawal <iramanagrawal@gmail.com>
2023-12-05 11:53:04 +0530

    code refactor @Navbar

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,11 +1,18 @@
+// Import necessary assets and styles
 import { closeIcon, menuIcon } from "../../assets";
-
-import {useState} from 'react'
+import { useState } from "react";
 import "./style.scss";
+interface Navigation {
+  name: string;
+  link: string;
+}
+// Functional component for Navbar
 const Navbar = () => {
+  // State for handling mobile navigation visibility
+  const [showNav, setShowNav] = useState(false);
 
-  const [showNav, setShowNav] = useState(false)
-  const navigations = [
+  // Array of navigation links and names
+  const navigations: Navigation[] = [
     {
       name: "Home",
       link: "#",
@@ -26,34 +33,47 @@ const Navbar = () => {
 
   return (
     <>
-    <nav>
-      <a href="#" className="logo">
-        Sound <span>DZign</span>
-      </a>
+      {/* Main navigation bar */}
+      <nav aria-label="Main Navigation" role="navigation">
+        {/* Logo */}
+        <a href="#" className="logo">
+          Sound <span>DZign</span>
+        </a>
 
-      <ul>
-        {navigations.map((nav, index) => (
-          <li key={index}>
-            <a href={nav.link}>{nav.name}</a>
-          </li>
-        ))}
-      </ul>
-      <div onClick={()=>setShowNav(true)} className="menu-icon">
-        <img src={menuIcon} alt="menu icon" />
+        {/* Navigation links */}
+        <ul>
+          {navigations.map((nav, index) => (
+            <li key={index}>
+              <a href={nav.link}>{nav.name}</a>
+            </li>
+          ))}
+        </ul>
+
+        {/* Mobile navigation icon */}
+        <div onClick={() => setShowNav(true)} className="menu-icon">
+          <img src={menuIcon} alt="Menu Icon" />
+        </div>
+      </nav>
+
+      {/* Mobile navigation menu */}
+      <div
+        className={`mobile-nav ${showNav ? "active" : ""}`}
+        aria-label="Mobile Navigation"
+        role="navigation"
+      >
+        {/* Close icon for mobile navigation */}
+        <div onClick={() => setShowNav(false)} className="close">
+          <img src={closeIcon} alt="Close Icon" />
+        </div>
+        {/* Mobile navigation links */}
+        <ul>
+          {navigations.map((nav, index) => (
+            <li key={index} onClick={() => setShowNav(false)}>
+              <a href={nav.link}>{nav.name}</a>
+            </li>
+          ))}
+        </ul>
       </div>
-    </nav>
-    <div className={`mobile-nav ${showNav ? 'active':""}`}>
-      <div onClick={()=>setShowNav(false)} className="close">
-        <img src={closeIcon} alt="closeicon" />
-      </div>
-      <ul>
-        {navigations.map((nav, index) => (
-          <li key={index} onClick={()=>setShowNav(false)}>
-            <a href={nav.link}>{nav.name}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
     </>
   );
 };
