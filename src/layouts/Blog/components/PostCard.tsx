0d47dbc787ff8817270a86ba86567328459a41ac
Raman Agrawal <iramanagrawal@gmail.com>
2023-12-05 12:41:11 +0530

    ±PostCard @Blog

--- src/layouts/Blog/components/PostCard.tsx
+++ src/layouts/Blog/components/PostCard.tsx
@@ -14,16 +14,22 @@ const PostCard: FC<PostCardI> = ({ post }) => {
   return (
     // Post container
     <article className="post">
-   
       <div className="tag">{post.tag}</div>
-   
+
       <a href="#" aria-label={post.title}>
-        
-        <img loading="lazy" src={post.img}  width="100%" alt={post.title} />
+        <img
+          loading="lazy"
+          src={post.img}
+          alt={post.title}
+          srcSet={`${post.img} 300w, ${post.img} 600w, ${post.img} 900w`}
+          sizes="(max-width: 600px) 300px,
+            (max-width: 900px) 600px,
+            900px"
+        />
+
       </a>
-     
+
       <a href="#" aria-label={post.title}>
-       
         <h3 className="post-title">{post.title}</h3>
       </a>
     </article>
